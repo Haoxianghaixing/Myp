494f6ae19cad657112ebcd2f6f5f19675e413e2e
0h_x0 <U202111997@hust.edu.cn>
2024-02-15 17:16:57 +0800

    img

--- src/mock/map.ts
+++ src/mock/map.ts
@@ -1,7 +1,7 @@
-import Img1 from 'C:/Project/demo/img/1.jpg'
-import Img2 from 'C:/Project/demo/img/2.jpg'
-import Img3 from 'C:/Project/demo/img/3.jpg'
-import Img4 from 'C:/Project/demo/img/4.jpg'
-import Img5 from 'C:/Project/demo/img/5.jpg'
+import Img1 from '/img/1.jpg'
+import Img2 from '/img/2.jpg'
+import Img3 from '/img/3.jpg'
+import Img4 from '/img/4.jpg'
+import Img5 from '/img/5.jpg'
 
 export const ImgList = [Img1, Img2, Img3, Img4, Img5]
